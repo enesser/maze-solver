8f7b19315a610af7ce16544f6ca0bc50dfa81d4c
Eric J Nesser <enesser@gmail.com>
2014-10-08 08:55:04 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/* globals process: true */
+/* globals require: true */
+/* globals console: true */
+
+/*
+if (process.argv.length < 3) {
+  console.log('Usage: node ' + process.argv[1] + ' FILENAME');
+  process.exit(1);
+}*/
+
+if (process.argv.length > 2) {
+    var fs = require('fs');
+    var mazeSolver = require('./mazeSolver');
+
+    process.argv.forEach(function(value, index) {
+        if (index > 1) {
+            fs.readFile(value, 'utf8', function(err, data) {
+                if (err) {
+                    throw err;
+                }
+                console.dir(mazeSolver(data));
+            });
+        }
+    });
+}
